c3a9639a145e1a831320b30d87b47674ba8ecb52
singh-anshuman <anshumansingh137@gmail.com>
2020-08-16 18:50:26 +0530

    Added test class for checking screen recording

--- src/main/java/com/flowtechgrowth/javaconcurrency/TestClass.java
+++ src/main/java/com/flowtechgrowth/javaconcurrency/TestClass.java
@@ -2,6 +2,10 @@ package com.flowtechgrowth.javaconcurrency;
 
 class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hey there !");
+		System.out.println("This is my first recording. Let's how this works out.");
+		
+		System.out.println("Hopefully this time only eclipse is going to be recorded in the final video !");
+		
+		System.out.println("Hey there. Lets see if this works.");
 	}
 }
